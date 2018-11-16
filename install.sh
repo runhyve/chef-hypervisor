bd32ff0e6b637e1ab6361c01d6eb862befc76181
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-11-16 10:18:44 +0100

    Refuse to run scripts without root permissions

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+if [ "$(id -u)" != 0 ]; then
+  echo "Error: This script needs root permissions"
+  exit 2
+fi
+
 if ! which chef-solo > /dev/null; then
   fetch  https://packages.chef.io/files/stable/chef/14.7.17/freebsd/11/chef-14.7.17_1.amd64.sh
   sh chef-14.7.17_1.amd64.sh
--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+if [ "$(id -u)" != 0 ]; then
+  echo "Error: This script needs root permissions"
+  exit 2
+fi
+
 cd cookbooks/hypervisor
 /opt/chef/embedded/bin/rake
