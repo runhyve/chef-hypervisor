e0a358ceae3c82e68dc1d0437f8d24b83451e02e
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-11-16 11:17:00 +0100

    Install qemu-utils

--- cookbooks/hypervisor/attributes/default.rb
+++ cookbooks/hypervisor/attributes/default.rb
@@ -1,4 +1,8 @@
-default['hypervisor']['packages'] = %w[webhook gotty jo jq bash dnsmasq gnu-ipcalc git netdata nginx]
+default['hypervisor']['packages'] = %w[webhook gotty jo jq bash dnsmasq gnu-ipcalc git netdata nginx qemu-utils]
 default['hypervisor']['runhyve_prefix'] = '/opt/runhyve'
 default['hypervisor']['repo']['vm-bhyve'] = 'https://github.com/runhyve/vm-bhyve.git'
 default['hypervisor']['repo']['vm-webhooks'] = 'https://gitlab.com/runhyve/vm-webhooks.git'
+default['hypervisor']['runhyve']['cloud-images'] = [
+'http://ftp.freebsd.org/pub/FreeBSD/releases/VM-IMAGES/11.2-RELEASE/amd64/Latest/FreeBSD-11.2-RELEASE-amd64.raw.xz',
+'https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-uefi1.img'
+]
