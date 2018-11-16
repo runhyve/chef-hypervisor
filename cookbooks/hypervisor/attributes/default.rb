d0b34e0330364e20250844251bfe3f62e914cc0a
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2018-11-16 11:46:28 +0100

    Install missing packages

--- cookbooks/hypervisor/attributes/default.rb
+++ cookbooks/hypervisor/attributes/default.rb
@@ -1,4 +1,5 @@
-default['hypervisor']['packages'] = %w[webhook gotty jo jq bash dnsmasq gnu-ipcalc git netdata nginx qemu-utils]
+default['hypervisor']['packages'] = %w[webhook gotty jo jq bash dnsmasq gnu-ipcalc git netdata nginx qemu-utils pwgen ts]
+
 default['hypervisor']['runhyve_prefix'] = '/opt/runhyve'
 default['hypervisor']['repo']['vm-bhyve'] = 'https://github.com/runhyve/vm-bhyve.git'
 default['hypervisor']['repo']['vm-webhooks'] = 'https://gitlab.com/runhyve/vm-webhooks.git'
