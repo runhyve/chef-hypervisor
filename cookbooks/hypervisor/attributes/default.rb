6b75f27a42b533094440cbf01612eb30f07a672e
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2019-01-15 22:07:46 +0100

    Add switch for choosing xip.io or fqdn in nginx configuration
    Ref. runhyve/webapp#49

--- cookbooks/hypervisor/attributes/default.rb
+++ cookbooks/hypervisor/attributes/default.rb
@@ -1,4 +1,4 @@
-default['hypervisor']['packages'] = %w[webhook gotty jo jq bash dnsmasq gnu-ipcalc git netdata nginx qemu-utils pwgen ts grub2-bhyve uefi-edk2-bhyvei vm-bhyve]
+default['hypervisor']['packages'] = %w[webhook gotty jo jq bash dnsmasq gnu-ipcalc git netdata nginx qemu-utils pwgen ts grub2-bhyve uefi-edk2-bhyve vm-bhyve]
 default['hypervisor']['runhyve_prefix'] = '/opt/runhyve'
 default['hypervisor']['repo']['vm-bhyve'] = 'https://github.com/runhyve/vm-bhyve.git'
 default['hypervisor']['repo']['vm-webhooks'] = 'https://gitlab.com/runhyve/vm-webhooks.git'
@@ -6,3 +6,6 @@ default['hypervisor']['runhyve']['cloud-images'] = [
 'http://ftp.freebsd.org/pub/FreeBSD/releases/VM-IMAGES/11.2-RELEASE/amd64/Latest/FreeBSD-11.2-RELEASE-amd64.raw.xz',
 'https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-uefi1.img'
 ]
+default['hypervisor']['use_xip'] = true
+
+default['hypervisor']['http_host'] = default['hypervisor']['use_xip'] ? "#{node['ipaddress']}.xip.io" : "#{node['fqdn']}"
