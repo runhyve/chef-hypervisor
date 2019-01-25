0f4f01b2b392eb8272a5df52b4cd0e98081f1644
Mateusz Kwiatkowski <mateusz@serveraptor.com>
2019-01-25 22:05:02 +0100

    Bump Chef version

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+CHEF_VERSION=14.9.13
+
 if [ "$(id -u)" != 0 ]; then
   echo "Error: This script needs root permissions"
   exit 2
 fi
 
 if ! which chef-solo > /dev/null; then
-  fetch  https://packages.chef.io/files/stable/chef/14.7.17/freebsd/11/chef-14.7.17_1.amd64.sh
-  sh chef-14.7.17_1.amd64.sh
+  fetch  https://packages.chef.io/files/stable/chef/${CHEF_VERSION}/freebsd/11/chef-${CHEF_VERSION}_1.amd64.sh
+  sh chef-${CHEF_VERSION}_1.amd64.sh
 fi
 
 git clone https://github.com/chef-cookbooks/chef_zfs.git ./cookbooks/chef_zfs
